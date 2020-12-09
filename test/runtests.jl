1110dadd2d9597112092722f02aeaf7415ea7c1f
Mark Redeman <markredeman@gmail.com>
2020-12-09 21:07:27 +0100

    Correctly read 2020 solutions when testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using AdventOfCodeSolutions
 using Test
 
 @testset "AdventOfCodeSolutions.jl" begin
-    foreach(include, readdir("2020/src/", join=true))
+    foreach(include, readdir("../2020/src/", join=true))
 end
