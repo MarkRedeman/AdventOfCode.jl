221384226c1babe6acb080dd7cabf72deb18cf8d
Mark Redeman <markredeman@gmail.com>
2020-12-09 17:36:51 +0100

    Solve day 9

---
+++ 2020/src/day_9.jl
@@ -0,0 +1,78 @@
+using Combinatorics
+using Test
+
+function input()
+    io = open("2020/data/day_9.txt", "r")
+    split(read(io, String), "\n", keepempty=false)
+end
+
+function parseInput(input)
+    parse.(Int, input)
+end
+
+
+function solvePart1(input, preambleSize)
+    numbers = map(parseInput, input)
+
+    for idx = (preambleSize + 1):length(input)
+        preamble = numbers[idx - preambleSize : idx - 1]
+
+        if (! (numbers[idx] in map(sum, combinations(preamble, 2))))
+            return numbers[idx]
+        end
+    end
+
+    nothing
+end
+
+function solvePart2(input, target)
+    numbers = map(parseInput, input)
+
+    for left = 1:length(numbers)
+        for right = left + 1 : length(numbers)
+            r = (numbers[left:right])
+            s = sum(r)
+            if (s > target)
+                break
+            end
+
+            if (s == target)
+                return min(r...) + max(r...)
+            end
+        end
+    end
+
+    return nothing
+end
+
+@testset "Day 9" begin
+    TEST = split("""
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+""", '\n', keepempty=false)
+
+    @test(solvePart1(TEST, 5) == 127)
+    # @test(solvePart1(input(), 25) == 10884537)
+
+    @test(solvePart2(TEST, 127) == 62)
+    @test(solvePart2(input(), 10884537) == 1261309)
+    # @test(solvePart2(input()) == 703)
+end
