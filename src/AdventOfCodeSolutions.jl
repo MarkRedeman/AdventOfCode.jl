075a6f985ba8779ca36ffd08db3feab81e378cf7
Mark Redeman <markredeman@gmail.com>
2020-12-10 19:47:50 +0100

    Allow opening input from 2019

--- src/AdventOfCodeSolutions.jl
+++ src/AdventOfCodeSolutions.jl
@@ -4,8 +4,8 @@ struct Puzzle{year, day, part} end
 Puzzle(day, part) = Puzzle{2020, day, part}()
 Puzzle(year, day, part) = Puzzle{year, day, part}()
 
-function openInput(::Puzzle{2020, d, p}) where {d, p}
-    return open(joinpath(@__DIR__, "../2020/data/day_$d.txt"), "r")
+function openInput(::Puzzle{n, d, p}) where {n, d, p}
+    return open(joinpath(@__DIR__, "../$n/data/day_$d.txt"), "r")
 end
 
 export Puzzle, openInput
